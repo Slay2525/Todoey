4b272480b6e7b1a2b4644534effd9e8ebaee1444
Slay2525 <garas43699@gmail.com>
2024-08-20 15:59:36 +0300

    final

--- lib/widgets/tasks_tile.dart
+++ lib/widgets/tasks_tile.dart
@@ -4,16 +4,18 @@ class TaskTile extends StatelessWidget {
   final bool isChecked;
   final String taskTile;
   final void Function(bool?) checkboxCallback;
+  final void Function() longPressCallback;
 
-  const TaskTile({
-    required this.isChecked,
-    required this.taskTile,
-    required this.checkboxCallback,
-  });
+  const TaskTile(
+      {required this.isChecked,
+      required this.taskTile,
+      required this.checkboxCallback,
+      required this.longPressCallback});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      onLongPress: longPressCallback,
       title: Text(
         taskTile,
         style: TextStyle(
